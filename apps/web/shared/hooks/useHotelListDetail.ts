859164538d21becff8de5ac683cd553128e9e36d
Rafael Nogueira <rafael.nogueira@gmail.com>
2025-03-13 16:15:12 -0300

    ✨ feat(web): update useHotelListDetail to utilize usePathname
    - Import usePathname from next/navigation to get the current pathname.
    - Modify handleCloseHotel to push the current pathname instead of a hardcoded '/' for better navigation consistency.

--- apps/web/shared/hooks/useHotelListDetail.ts
+++ apps/web/shared/hooks/useHotelListDetail.ts
@@ -1,10 +1,11 @@
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 import { useToggle } from '@hooks/useToggle';
 
 export const useHotelListDetail = () => {
   const { isEnabled, toggle } = useToggle();
   const { push } = useRouter();
+  const pathname = usePathname();
 
   const handleOpenHotel = (hotelId: number) => {
     push(`?h=${hotelId}`, {
@@ -14,7 +15,7 @@ export const useHotelListDetail = () => {
   };
 
   const handleCloseHotel = () => {
-    push('/', {
+    push(pathname, {
       scroll: false,
     });
     toggle();
