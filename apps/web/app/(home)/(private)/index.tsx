7266d2f61463826acba511da64879a6a4e03b8ac
Rafael Nogueira <rafael.nogueira@gmail.com>
2025-03-31 16:06:52 -0300

    feat(Auth): implement page authentication and private routing
    - Added `usePageAuth` hook to manage token retrieval from search parameters.
    - Updated `Page` component to conditionally render `HomePrivate` or `HomePublic` based on the presence of a token.
    - Enhanced `HomePrivate` component with a structured layout including `HeaderPrivate` and main content area.
    - Modified `HomePublic` to pass dynamic `MENU_ITEMS` to `UserMobileNavigation` for improved menu flexibility.

--- apps/web/app/(home)/(private)/index.tsx
+++ apps/web/app/(home)/(private)/index.tsx
@@ -1,3 +1,17 @@
-export const HomePrivate = () => {
-  return <div>Home Logada</div>;
-};
+import { HeaderPrivate } from '@components/header';
+
+import { Container } from '@ui/components/layouts/container';
+
+export function HomePrivate() {
+  return (
+    <>
+      <Container as="section" className="relative h-screen">
+        <HeaderPrivate />
+      </Container>
+
+      <Container as="main" className="relative z-10 bg-background">
+        <div>Conteudo</div>
+      </Container>
+    </>
+  );
+}
--- apps/web/app/(home)/page.tsx
+++ apps/web/app/(home)/page.tsx
@@ -1,5 +1,14 @@
+'use client';
+import { HomePrivate } from './(private)';
 import { HomePublic } from './(public)';
+import { usePageAuth } from './page-auth';
 
 export default function Page() {
+  const { token } = usePageAuth();
+
+  if (token) {
+    return <HomePrivate />;
+  }
+
   return <HomePublic />;
 }
