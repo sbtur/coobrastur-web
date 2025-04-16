b82da31079cd57d6511851ade06a8e52d5e522af
Chelder <chelder.guimaraes@viajarfazbem.com>
2025-04-16 18:05:31 -0300

    feat(auth): substituir ícone de avião por ícone de categoria no layout de autenticação
    - Removido o ícone de avião do layout de autenticação e substituído pelo ícone de categoria, melhorando a identidade visual da página.
    - A mudança contribui para uma apresentação mais coesa e alinhada com a marca.

--- apps/web/app/(auth)/layout.tsx
+++ apps/web/app/(auth)/layout.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 
+import CategoryIcon from '@images/icons/coobrastur-logo.svg';
+
 export default function AuthLayout({
   children,
 }: {
@@ -19,11 +21,8 @@ export default function AuthLayout({
       <div className="flex-1 px-[10px] lg:px-[150px] flex items-center justify-center">
         <div className="flex flex-col gap-4 pb-2 lg:text-left w-full max-w-[760px] lg:pt-0 pt-10">
           <div className="flex justify-center lg:hidden">
-            <Image
-              src="/images/pages/login/airplane-icon.svg"
-              alt="Icone"
-              width={55}
-              height={55}
+            <CategoryIcon
+              className={`text-primary-300 hover:scale-105 transition-all duration-300`}
             />
           </div>
           {children}
