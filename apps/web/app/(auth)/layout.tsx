aee3d7133fd4057059c4ca085312babe94a71197
Chelder <chelder.guimaraes@viajarfazbem.com>
2025-04-17 09:09:13 -0300

    feat(auth): atualizar layout de autenticação com novos componentes e parceiros
    - Substituído o componente de imagem padrão pelo componente personalizado '@components/image' para melhor controle e otimização.
    - Adicionada a exibição de parceiros na parte inferior do layout, utilizando dados do arquivo '@utils/data/partners', melhorando a apresentação visual e a identidade da marca.
    - Atualizada a imagem de fundo do layout para um novo arquivo, garantindo uma estética mais alinhada com a proposta da página.

--- apps/web/app/(auth)/layout.tsx
+++ apps/web/app/(auth)/layout.tsx
@@ -1,4 +1,5 @@
-import Image from 'next/image';
+import { Image } from '@components/image';
+import { PARTNERS } from '@utils/data/partners';
 
 import CategoryIcon from '@images/icons/coobrastur-logo.svg';
 
@@ -12,11 +13,25 @@ export default function AuthLayout({
       <div className="relative lg:h-full w-full lg:block">
         <Image
           src="/images/pages/login/grupo.png"
-          alt="Login"
+          alt="Logo"
           fill
-          className="object-cover"
+          className="object-cover object-[33%_50%] fixed inset-0 -z-10"
           priority
+          quality={100}
         />
+
+        <div className="hidden lg:flex w-full lg:w-[618px] mx-auto absolute bottom-10 left-1/2 -translate-x-1/2 -translate-y-1/2 items-center gap-4 p-5 rounded-md bg-white shadow-lg justify-around">
+          {PARTNERS.map(partner => (
+            <div key={partner.alt} className="max-w-[130px]">
+              <Image
+                {...partner}
+                width={130}
+                height={30}
+                className="w-full h-full object-contain"
+              />
+            </div>
+          ))}
+        </div>
       </div>
       <div className="flex-1 px-[10px] lg:px-[150px] flex items-center justify-center">
         <div className="flex flex-col gap-4 pb-2 lg:text-left w-full max-w-[760px] lg:pt-0 pt-10">
