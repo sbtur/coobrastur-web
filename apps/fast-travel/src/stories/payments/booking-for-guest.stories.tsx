ca8173217822c0bd2e76c17d2554f44e1e62d98b
Chelder <chelder.guimaraes@viajarfazbem.com>
2025-04-24 14:49:56 -0300

    CU-86a84wu4t - Componente - Convidado (terceiro) sem cadastro

---
+++ apps/fast-travel/src/stories/payments/booking-for-guest.stories.tsx
@@ -0,0 +1,43 @@
+import { BookingForGuest } from '../../../../web/app/(pages)/checkout/components/dependents/booking-for-guest';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof BookingForGuest> = {
+  title: 'Payments/BookingForGuest',
+  component: BookingForGuest,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof BookingForGuest>;
+
+export const Default: Story = {
+  args: {},
+};
+
+export const WithFirstDegreeRelative: Story = {
+  args: {},
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Exibe o componente com a opção de parente de primeiro grau selecionada',
+      },
+    },
+  },
+};
+
+export const WithThirdDegreeRelative: Story = {
+  args: {},
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Exibe o componente com a opção de parente de terceiro grau selecionada',
+      },
+    },
+  },
+};
