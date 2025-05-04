efdefac92f14756198bbb1ac201e55b8bae07e4d
Chelder <chelder.guimaraes@viajarfazbem.com>
2025-05-04 08:38:58 -0300

    feat: adicionar componentes CarousselPrivate e CarrouselDetails
    - Implementação do componente CarousselPrivate, que exibe um carrossel de imagens com informações e botões interativos.
    - Criação do componente CarrouselDetails, que apresenta detalhes sobre planos e reservas.
    - Utilização de componentes do design system (@coobrastur/ui) para garantir consistência visual e responsividade.
    - Estrutura modular para facilitar a manutenção e reutilização dos componentes.

---
+++ apps/web/app/(home)/(private)/containers/carrousel-details.tsx
@@ -0,0 +1,50 @@
+import { Calendar, CalendarCheck } from 'lucide-react';
+
+import { Text } from '@coobrastur/ui/components/data-display/text';
+
+export function CarrouselDetails() {
+  return (
+    <div className="flex flex-col h-full">
+      <div className="w-full h-full bg-primary-200 rounded-lg shadow-sm flex flex-col relative px-7 py-8 gap-1">
+        <Text size="lg" className="text-white font-bold">
+          Meus Planos
+        </Text>
+        <Text size="sm" className="text-white">
+          Você tem mais de um plano ativo.
+        </Text>
+        <Text size="sm" className="text-white font-bold underline">
+          Confira suas opções
+        </Text>
+      </div>
+      <div className="w-full h-full bg-primary-100 rounded-lg shadow-sm flex flex-col relative -mt-[20px] px-7 py-8">
+        <Text size="lg" className="text-white font-bold">
+          R$ 200,00
+        </Text>
+        <Text size="sm" className="text-white">
+          Lorem ipsum dolor sit
+        </Text>
+        <Text size="sm" className="text-white font-bold underline">
+          Saiba mais
+        </Text>
+      </div>
+      <div className="w-full h-full bg-secondary-200 rounded-lg shadow-sm flex flex-col relative -mt-[20px] px-7 py-8 gap-2">
+        <Text size="lg" className="text-primary-300 font-bold truncate">
+          Sky Borges Hotel Alpenharus
+        </Text>
+        <div className="flex gap-6">
+          <div className="flex items-center gap-2">
+            <Calendar size={16} />
+            10/05/2025
+          </div>
+          <div className="flex items-center gap-2">
+            <CalendarCheck size={16} />
+            10/05/2025
+          </div>
+        </div>
+        <Text size="sm" className="text-primary-100 font-bold underline">
+          Ver Voucher
+        </Text>
+      </div>
+    </div>
+  );
+}
